e91621dc351fccb26b5ba0013adc38777c06bd6e
LeoDLeao <leo.riegler1@gmail.com>
2020-07-27 10:58:21 -0300

    Toolbar

--- app/src/main/java/com/example/zap/activities/MainActivity.java
+++ app/src/main/java/com/example/zap/activities/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.zap.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import com.example.zap.R;
+import com.example.zap.firebase.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Whatsapp");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.menuSair:
+
+                deslogarUsuario();
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+
+        FirebaseAuth auth = ConfiguracaoFirebase.getFirebaseAuth();
+        try{
+            auth.signOut();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
