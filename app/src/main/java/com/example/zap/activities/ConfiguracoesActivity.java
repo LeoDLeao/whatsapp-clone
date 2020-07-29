4c6e340057695e62e54e6f1bf715f7bdc9348d52
LeoDLeao <leo.riegler1@gmail.com>
2020-07-29 18:31:19 -0300

    configuracao foto

--- app/src/main/java/com/example/zap/activities/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/zap/activities/ConfiguracoesActivity.java
@@ -2,10 +2,18 @@ package com.example.zap.activities;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,7 +21,15 @@ import androidx.appcompat.widget.Toolbar;
 import com.example.zap.R;
 import com.example.zap.helper.Permissoes;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private CircleImageView circleImageViewPerfil;
+
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
 
     private String[] permissoesNecessarias = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -27,11 +43,71 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         Permissoes.validarPermissoes(permissoesNecessarias,this,1);
 
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
+        circleImageViewPerfil = findViewById(R.id.imageFoto);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.titulo_configuracoes);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageButtonCamera.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if(intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, SELECAO_CAMERA);
+            }
+        });
+
+        imageButtonGaleria.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            if(intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, SELECAO_GALERIA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try {
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+
+
+                        break;
+
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                            imagem = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(),localImagemSelecionada));
+                        }else{
+                            MediaStore.Images.Media.getBitmap(getContentResolver(),localImagemSelecionada);
+                        }
+
+                        break;
+                }
+
+                if( imagem != null){
+
+                    circleImageViewPerfil.setImageBitmap(imagem);
+
+
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
